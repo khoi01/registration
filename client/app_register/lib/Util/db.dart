aa004b58cf54c0bf12468c3639a35bf2129bf695
Abdul Qayyum <khoi00091@gmail.com>
2020-11-20 09:18:41 +0800

    Update db.dart

--- client/app_register/lib/Util/db.dart
+++ client/app_register/lib/Util/db.dart
@@ -2,33 +2,32 @@ import 'dart:async';
 import 'package:app_register/models/db/item/item.dart';
 import 'package:sqflite/sqflite.dart';
 
-
-abstract class DB{
+abstract class DB {
   static Database _db;
   static int get _version => 1;
 
-  static Future<void> init() async{
-    if(_db != null){return;}
+  static Future<void> init() async {
+    if (_db != null) {
+      return;
+    }
 
-    try{
-      String _path = await getDatabasesPath()+"exampleDB";
-      _db = await openDatabase(_path,version:_version,onCreate: onCreate);
-
-    }catch(ex){
+    try {
+      String _path = await getDatabasesPath() + "exampleDB";
+      _db = await openDatabase(_path, version: _version, onCreate: onCreate);
+    } catch (ex) {
       print(ex);
     }
   }
 
-
-  	static Database db(){
+  static Database db() {
     init();
-		return _db;
-	}
+    return _db;
+  }
 
-  	static void onCreate(Database db, int version) async{
-      print("Database:Initialize Table..");
-      print("Database: Version $_version");
-		await db.execute('CREATE TABLE ${Item.table} (id INTEGER PRIMARY KEY NOT NULL, name STRING)');
-    }
-
+  static void onCreate(Database db, int version) async {
+    print("Database:Initialize Table..");
+    print("Database: Version $_version");
+    await db.execute(
+        'CREATE TABLE ${Item.table} (id INTEGER PRIMARY KEY NOT NULL, name STRING)');
+  }
 }
